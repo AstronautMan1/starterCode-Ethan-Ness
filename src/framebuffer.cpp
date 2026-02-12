f6c6b7855295d798c2eaccb879d79d8e05df2306
AstronautMan1 <ethanness49@gmail.com>
2026-02-12 17:56:03 -0600

    added comments explaining the LSB steganography more

--- src/framebuffer.cpp
+++ src/framebuffer.cpp
@@ -93,6 +93,20 @@ void Framebuffer::exportToPNG(std::string filename){
     imData.write(filename);
 }
 
+
+/*
+    Summary of the LSB encoding and decoding
+
+    encoding: message bytes -> extracts bits --> embed the buts in LSB of sequential color channels
+
+    header ecoding: first 32 channels are for storing the message file size (header) later for decoding
+
+    Decoding: we read first 32 channels for file size -> read file size in bits -> then reconstruct the bytes from the bits
+
+    Storage: we use floating point (0.0-1.0) for normalized colors but have to convert them to 0-255 for png color handling and bit manipulation
+*/
+
+
 /// @brief This encodes a message to an image for steganography hiding
 /// @param messageFile the name of the message file we are using as payload
 void Framebuffer::encodeMessage(const std::string &messageFile) {
@@ -100,7 +114,7 @@ void Framebuffer::encodeMessage(const std::string &messageFile) {
     std::ifstream file(messageFile, std::ios::binary); // loads the message file in binary form
     if (!file) return; // if we can't make a file in binary exit
 
-    std::vector<unsigned char> data((std::istreambuf_iterator<char>(file)), {}); // load the entire file into a vector of bytes each element in vector represents 8 bits 
+    std::vector<unsigned char> data((std::istreambuf_iterator<char>(file)), {}); // load the entire file into a vector of 8 bit bytes each element in vector represents 8 bits 
     file.close(); // close file 
 
     uint32_t fileSize = static_cast<uint32_t>(data.size()); // the number of bytes in the message file
@@ -115,7 +129,7 @@ void Framebuffer::encodeMessage(const std::string &messageFile) {
 
         int val = static_cast<int>(std::round(fbstorage[pixelIdx][channelIdx] * 255.0f)); // go from floating point 0 - 1 to 0 - 255 and round up so we don't lose our LSB 
         
-        val = (val & ~1) | bit; // makes the last bit of the color channel 0 then we plug in the message bit of 0 or 1 in its place
+        val = (val & ~1) | bit; // makes the last bit of the color channel a 0 bit then we plug in the message bit of 0 or 1 in its place with the | bit (example if val from that pixels color channel is 173 10101101 and the bit for message is 0 it turns to 172 10101100)
         fbstorage[pixelIdx][channelIdx] = val / 255.0f; // turns modified color back to floating point and saves it in the framebuffer
 
         if (++channelIdx > 2) { // if we are outside the color channel 
@@ -124,14 +138,14 @@ void Framebuffer::encodeMessage(const std::string &messageFile) {
         }
     };
 
-    for (int i = 31; i >= 0; --i) { // add the header
-        embedBit((fileSize >> i) & 1); // we shift the fileSize bits over to the right by one for the 32 bit header tells decoder where the message ends
+    for (int i = 31; i >= 0; --i) { // encode the header
+        embedBit((fileSize >> i) & 1); // we loop from 31 down to 0 and shift the file size right by i then extract the bit at position i and each but goes into the next color channel (32 channels total ~11 pixels) (example if file size is 5 then binary 00000101 is embeded)
     }
 
     
     for (unsigned char byte : data) {
-        for (int i = 7; i >= 0; --i) { // for every 8 bits in the data file we hide each bit
-            embedBit((byte >> i) & 1); // we embed our message bit into the image
+        for (int i = 7; i >= 0; --i) { 
+            embedBit((byte >> i) & 1); // loop from bit 7 to bit 0 and extract each bit and embed it sequentially
         }
     }
 }
@@ -143,7 +157,7 @@ void Framebuffer::decodeMessage(const std::string &outputFile) {
     std::ofstream outFile(outputFile, std::ios::binary); // creates our output file and tells the data written to it should be binary not text
     if (!outFile) return; // if the file isn't created properly return
 
-    uint32_t fileSize = 0; // result of the first 32 color channels which is the header of the encoded payload
+    uint32_t fileSize = 0; // file size tracker
     int pixelIdx = 0; // pixelindex for which pixel we are looking at
     int channelIdx = 0; // channel index to show if we are looking at the red, blue, or green channel
 
@@ -151,9 +165,9 @@ void Framebuffer::decodeMessage(const std::string &outputFile) {
     for (int i = 31; i >= 0; --i) {
         int val = static_cast<int>(std::round(fbstorage[pixelIdx][channelIdx] * 255.0f)); // converts from floating point 0-1 to integer color 0-255 and we round up so we don't lose our least significant bit
     
-    // this checks if the least significant bit is odd if it is then we bitshift by i to turn on that bit in our fileSize variable
+    // this extracts the LSB and if it is 1 we set the bit at position i in fileSize 
         if (val & 1) {
-            fileSize |= (1U << i); // bit shifts the odd bit in the fileSize variable by i
+            fileSize |= (1U << i); // adds the bit at position i (example if header LSBs are 0,0,0,0,0,1,0,1 we get filesize = 5)
         }
 
         if (++channelIdx > 2) { channelIdx = 0; pixelIdx++; } // this moves us through the image we check red,green, and blue then we reset the channel and go to the next pixel
@@ -163,7 +177,7 @@ void Framebuffer::decodeMessage(const std::string &outputFile) {
     int bitCount = 0; // bit count
     
     // We check if the size of the data is within bound of our image
-    uint32_t maxPossibleBytes = (uint32_t)((fbstorage.size() * 3 - 32) / 8); // calculates the max amount of data the image can have
+    uint32_t maxPossibleBytes = (uint32_t)((fbstorage.size() * 3 - 32) / 8); // calculates the max amount of data the image can have which is width * height of pixels * 3 for color channels - 32 for header / 8 to get the bytes
     if (fileSize > maxPossibleBytes) { // if the file size is greater then max we return and print out an error
         std::cerr << "Error: Decoded file size (" << fileSize << ") exceeds image capacity!" << std::endl;
         return;
@@ -172,13 +186,17 @@ void Framebuffer::decodeMessage(const std::string &outputFile) {
     for (uint32_t i = 0; i < fileSize * 8; ++i) { // loop through every bit of the file (byte * 8)
         if (pixelIdx >= fbstorage.size()) break; // the pixel index is greater then the size of the framebuffer storage we break
 
-        int val = static_cast<int>(std::round(fbstorage[pixelIdx][channelIdx] * 255.0f)); // cast from floating point 0-1 to 0 - 255 the pixel and channel we are at
-        currentByte = (currentByte << 1) | (val & 1); // we take our byte of 8 bits shift them to the left and add the bit we found earlier val into the last spot
+        int val = static_cast<int>(std::round(fbstorage[pixelIdx][channelIdx] * 255.0f)); // cast from floating point 0-1 to 0 - 255 the pixel and channel we are at and get that bit 
+        currentByte = (currentByte << 1) | (val & 1); // shift the existing bits left to add the new bit to the right of our output 
         bitCount++; // add to bit count
+        // example outupt current byte = 00000000
+        // after bit 0 (LSB = 0) = 00000000
+        // after bit 1 (LSB = 1) = 00000001
+        // after bit 2 (LSB = 0) = 00000010 and so on...
 
         if (bitCount == 8) { // when we reach bitCount 8 that is one character so we add it to the output file and reset the current byte and bitcount for the next character
             outFile.put(currentByte); // we add the byte to the output file which is one character
-            currentByte = 0; // reset current byte
+            currentByte = 0; // reset current byte for next byte
             bitCount = 0; // reset bit count
         }
 
@@ -190,7 +208,8 @@ void Framebuffer::decodeMessage(const std::string &outputFile) {
 /// @brief This loads in the png image into a framebuffer for future handling
 /// @param filename name of the png file
 void Framebuffer::loadFromPNG(std::string filename) {
-    png::image<png::rgb_pixel> image(filename);
+
+    png::image<png::rgb_pixel> image(filename); // load png image
     
     // Update dimensions to match the loaded file
     width = image.get_width(); // set the width from the image
